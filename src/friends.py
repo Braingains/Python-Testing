6f876a5fa69212ab8e3ed1d0a4d601677a871c9d
Ewan Brain <ewan.brain@gmail.com>
2020-11-19 13:28:47 +0000

    post lunch commit

--- src/friends.py
+++ src/friends.py
@@ -4,4 +4,18 @@ def get_name (person):
     return person ["name"]
 
 def get_favourite_tv_show (person):
-    return ["favourites"]["tv_show"]
+    return person["favourites"]["tv_show"]
+
+def likes_to_eat (person, food):
+    for snack in (person["favourites"]["snacks"]):
+        if snack == food:
+            return True
+    else: 
+            return False
+
+
+def add_friend (person, name):
+    person["friends"].append(name)
+
+def remove_friend (person, name):
+    person["friends"].remove(name)
